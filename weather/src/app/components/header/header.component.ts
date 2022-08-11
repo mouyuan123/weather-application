c2aee06e11a5f792ca1da04f4a286b20d4b7303c
mouyuan123 <mouyuancheng2@gmail.com>
2022-08-11 13:47:01 +0800

    GOGO

--- weather/src/app/components/header/header.component.ts
+++ weather/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   // Allows me to toggle between light & dark mode
   @Output() toggleMode = new EventEmitter();
   // Check whether to change to dark mode
-  isDarkMode: boolean = false;
+  @Input() isDarkMode!: boolean;
 
   constructor() { }
 
